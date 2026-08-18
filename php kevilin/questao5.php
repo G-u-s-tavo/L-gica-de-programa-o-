a1bb425bf57b7825e1d428148e01c605214496aa
Gustavo <luiz.santos420@aluno.ce.gov.br>
2026-08-18 07:56:33 -0300

    Add files via upload

--- php kevilin/questao5.php
+++ php kevilin/questao5.php
@@ -1,6 +1,15 @@
 <?php
 
-$salario = 900;
-$horas =5; 
+$salario = 1500;
+$horastr = 2; // horas que foram trabalhadas
+$hora = 5/100*$salario; // valor da hora  
+$imposto = 5/100*$salario and 10/100*$salario and 20/100*$salario;
+ 
+    if ($salario <=900){
+    echo $salario + $horastr*$hora;
+} elseif ($salario >=1500){
+    $imposto = 5/100*$salario;
+    echo $salario + $horastr*$hora - $imposto ;}
 
-if 
+
+?>
